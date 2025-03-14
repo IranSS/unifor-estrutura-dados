13b448619d69a266641f3c8beee2bcc5bf4f2bbd
Iran <iranildosousa1234@gmail.com>
2025-03-14 10:50:57 -0300

    Comentários desnecessários apagados

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,8 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*
 
-        //Somente para testar a funcionalidade da lista encadeada
-        ManipuladorLista manipuladorLista = new ManipuladorLista();
-        No elemento1 = new No(10);
-        No elemento2 = new No(20);
-        No elemento3 = new No(30);
-        No elemento4 = new No(40);
-        No elemento5 = new No(80);
-
-        Lista minhaLista = manipuladorLista.CriarLista();
-
-        manipuladorLista.AdicionarElemento(minhaLista, elemento1);
-        manipuladorLista.AdicionarElemento(minhaLista, elemento2);
-        manipuladorLista.AdicionarElemento(minhaLista, elemento3);
-        manipuladorLista.AdicionarElemento(minhaLista, elemento4);
-        manipuladorLista.AdicionarElemento(minhaLista, elemento5);
-
-        manipuladorLista.Imprimir(minhaLista);
-         */
         MyHash hashtable = new MyHash();
-
-//        hashtable.adicionarPalavra("Arroz");
-//        hashtable.adicionarPalavra("Feijão");
-//        hashtable.adicionarPalavra("Macarrão");
-//        hashtable.adicionarPalavra("Linguiça");
-//        hashtable.adicionarPalavra("Bife");
-//        hashtable.adicionarPalavra("Cebola");
-//        hashtable.adicionarPalavra("Melancia");
-//
-//        System.out.println("Tem arroz na tabela? " + hashtable.buscarPalavra("Arroz"));
-//        System.out.println("Tem linguiça na tabela? " + hashtable.buscarPalavra("Linguiça"));
-//
-//        hashtable.removerPalavra("Linguiça");
-//
-//        System.out.println("Tem linguiça na tabela? " + hashtable.buscarPalavra("Linguiça"));
 
             IndiceRemissivo indice = new IndiceRemissivo();
             // Processa o arquivo de texto
